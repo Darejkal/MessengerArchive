970211dbffffeeabf2bb6dc6e7945307c071ba83
An <55143337+Darejkal@users.noreply.github.com>
2023-02-23 22:25:27 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+//javascript: 
+(function run() {
+  let all = document.querySelectorAll('div[aria-label="Menu"]');
+  if (all.length == 0) return;
+  let a = all[0];
+  a.click();
+  setTimeout(() => {
+    document.querySelectorAll("div[role=menuitem]").forEach((act) => {
+      if (act.innerText.match(/Archive chat/)) act.click();
+    });
+    run();
+  }, 500);
+})();
